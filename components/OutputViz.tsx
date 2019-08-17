4ed9fd1d1163bb9890c623715f831b6f5ce66391
Simon Fishel <sfishel@splunk.com>
2019-08-17 16:10:48 -0700

    adds basic visualization logic for output

--- components/OutputViz.tsx
+++ components/OutputViz.tsx
@@ -1,3 +1,5 @@
+import { scaleLinear } from 'd3-scale';
+import { find, flatten, get, map, max } from 'lodash';
 import * as React from 'react';
 
 interface Props {
@@ -6,6 +8,53 @@ interface Props {
 
 export default class extends React.Component<Props> {
   public render() {
-    return <pre>{JSON.stringify(this.props.output, null, 2)}</pre>;
+    const { output } = this.props;
+    if (!output || output.length === 0) {
+      return null;
+    }
+    const maxTimeValue = max(
+      flatten(map(output, stream => stream.events.map(event => event.timestamp))),
+    );
+    const xScale = scaleLinear()
+      .domain([0, maxTimeValue * 1.1])
+      .range([0, 700])
+      .clamp(true);
+
+    return (
+      <svg width={700} height={500}>
+        {output.map((stream, i) => {
+          const completionTime = get(
+            find(stream.events, { type: 'complete' }),
+            'timestamp',
+            Infinity,
+          );
+          return (
+            <g key={stream.name} transform={`translate(0, ${100 * i})`}>
+              <text dominantBaseline="hanging">{stream.name}</text>
+              <path strokeWidth={1} stroke="black" d={`M 0 50 L ${xScale(completionTime)} 50`} />
+              {stream.events
+                .filter(event => event.type === 'value' || event.type === 'error')
+                .map(event => (
+                  <g transform={`translate(${xScale(event.timestamp)}, 50)`}>
+                    {event.type === 'value' && (
+                      <>
+                        <circle r={25} fill="green" />
+                        <text dominantBaseline="middle" textAnchor="middle">
+                          {event.value}
+                        </text>
+                      </>
+                    )}
+                    {event.type === 'error' && (
+                      <>
+                        <circle r={25} fill="red" />
+                      </>
+                    )}
+                  </g>
+                ))}
+            </g>
+          );
+        })}
+      </svg>
+    );
   }
 }
