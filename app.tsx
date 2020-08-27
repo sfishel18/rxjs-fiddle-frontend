37c17090f6d1efe5ab84d99ff10ca8f94f870ba6
Simon Fishel <simonf@gmail.com>
2020-08-26 20:17:15 -0700

    wip

--- app.tsx
+++ app.tsx
@@ -1,9 +1,28 @@
+import cookie from 'js-cookie';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
+import { BatchRecorder, jsonEncoder } from 'zipkin';
+import Tracer from 'zipkin-javascript-opentracing';
+import { HttpLogger } from 'zipkin-transport-http';
 import AppContainer from './redux/containers/AppContainer';
 import createStore from './redux/create-store';
 import services from './redux/services';
+
+const tracer = new Tracer({
+  kind: 'client',
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: `${cookie.get('API_URL')}/v1/trace`,
+      jsonEncoder: jsonEncoder.JSON_V2,
+    }),
+  }),
+  serviceName: 'frontend',
+});
+
+const span = tracer.startSpan('aha');
+
+setTimeout(() => span.finish(), 5000);
 
 const store = createStore(undefined, services);
 
