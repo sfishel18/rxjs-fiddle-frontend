8f09335d9b10cf9ad8d739b100b91d3bd6953f64
Simon Fishel <simonf@gmail.com>
2020-09-25 21:48:28 -0700

    end-to-end tracing example

--- api.ts
+++ api.ts
@@ -1,18 +1,43 @@
 import cookie from 'js-cookie';
+import { BatchRecorder, jsonEncoder } from 'zipkin';
+import Tracer, { FORMAT_HTTP_HEADERS } from 'zipkin-javascript-opentracing';
+import { HttpLogger } from 'zipkin-transport-http';
 import { FiddleOutput } from './types';
 
 const API_URL = cookie.get('API_URL');
 
-const fetchFiddleOutput = (source: string): Promise<FiddleOutput> =>
-  fetch(`${API_URL}/run-fiddle`, {
-    body: JSON.stringify({ source }),
-    headers: new Headers({
-      'Content-Type': 'application/json',
+const tracer = new Tracer({
+  kind: 'client',
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: `${API_URL}/v2/trace`,
+      jsonEncoder: jsonEncoder.JSON_V2,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }),
+  }),
+  serviceName: 'frontend',
+});
+
+const fetchFiddleOutput = (source: string): Promise<FiddleOutput> => {
+  const span = tracer.startSpan('fetchFiddleOutput');
+  span.setTag('environment', 'sfishel');
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  tracer.inject(span, FORMAT_HTTP_HEADERS, headers);
+  return fetch(`${API_URL}/run-fiddle`, {
+    body: JSON.stringify({ source }),
+    headers,
     method: 'POST',
   })
     .then(response => response.json())
-    .then(json => json.response);
+    .then(json => {
+      span.finish();
+      return json.response;
+    });
+};
 
 const api = { fetchFiddleOutput };
 
--- app.tsx
+++ app.tsx
@@ -1,31 +1,8 @@
-import cookie from 'js-cookie';
 import React from 'react';
 import ReactDom from 'react-dom';
-import { RecoilRoot } from 'recoil';
-import { BatchRecorder, jsonEncoder } from 'zipkin';
-import Tracer from 'zipkin-javascript-opentracing';
-import { HttpLogger } from 'zipkin-transport-http';
 import api from './api';
 import AppContainer from './containers/AppContainer';
 import AtomsStore from './stores/atoms-store';
-
-const tracer = new Tracer({
-  kind: 'client',
-  recorder: new BatchRecorder({
-    logger: new HttpLogger({
-      endpoint: `${cookie.get('API_URL')}/v1/trace`,
-      jsonEncoder: jsonEncoder.JSON_V2,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }),
-  }),
-  serviceName: 'frontend',
-});
-
-const span = tracer.startSpan('aha');
-
-setTimeout(() => span.finish(), 5000);
 
 // tslint:disable-next-line: no-any
 (ReactDom as any).unstable_createRoot(document.getElementById('app')).render(
